4eb39c7483083b1152b4b897f214907ac0897a84
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-04-19 20:59:32 +0500

    mapping basic urls for testing

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,46 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
+class Author(models.Model):
+    username = models.CharField(max_length=255,unique=True)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_staff = models.BooleanField()
+    about = models.TextField()
 
+class Post(models.Model):
+    POST_STATUS_DRAFT='d'
+    POST_STATUS_PUBLISHED= 'P'
+    POST_STATUS_DELETED = 'D'
+    POST_STATUS_CHOICES = [
+        (POST_STATUS_DRAFT,'draft'),
+        (POST_STATUS_PUBLISHED,'published'),
+        (POST_STATUS_DELETED,'deleted')
+    ]
+    title = models.CharField(max_length=255)
+    content =  models.TextField()
+    author = models.ForeignKey(Author,on_delete=models.PROTECT,related_name='Author')
+    publish_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+                        max_length=1,
+                        choices=POST_STATUS_CHOICES,
+                        default=POST_STATUS_DRAFT)
+    slug = models.SlugField(unique=True,max_length=255)
 
+class Comment(models.Model):
+    COMMENT_STATUS_APPROVED = 'A'
+    COMMENT_STATUS_PENDING = 'P'
+    COMMENT_STATUS_SPAM = 'S'
+
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_APPROVED,'approved'),
+        (COMMENT_STATUS_PENDING,'pending'),
+        (COMMENT_STATUS_SPAM,'spam')
+    ]
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    author = models.CharField(max_length=255)
+    email = models.EmailField()
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1,choices=COMMENT_STATUS,default=COMMENT_STATUS_PENDING)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def say_hello(request):
+    return render(request,'index.html',{'name':'Waqar'})
